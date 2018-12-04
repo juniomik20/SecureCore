d13c35390f1a88b32097700375af084994784bbe
Marc <43479593+MORATA22@users.noreply.github.com>
2018-12-04 18:27:06 +0100

    Update Filiations.Designer.cs

--- WookieTablas/Filiations.Designer.cs
+++ WookieTablas/Filiations.Designer.cs
@@ -28,10 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.sdstxtDescFiliations = new SdsTexBox.SdsTexBox();
-            this.sdstxtCodeFiliation = new SdsTexBox.SdsTexBox();
-            this.labDescFiliations = new System.Windows.Forms.Label();
-            this.labCodeFiliation = new System.Windows.Forms.Label();
+            this.sdsTexBox1 = new SdsTexBox.SdsTexBox();
+            this.sdsTexBox2 = new SdsTexBox.SdsTexBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sdsTexBox3 = new SdsTexBox.SdsTexBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnUpdate
@@ -42,78 +44,109 @@
             // 
             this.btnInsert.Location = new System.Drawing.Point(57, 400);
             // 
-            // sdstxtDescFiliations
+            // sdsTexBox1
             // 
-            this.sdstxtDescFiliations.BackColor = System.Drawing.Color.LightGreen;
-            this.sdstxtDescFiliations.CodeSector = null;
-            this.sdstxtDescFiliations.ColumnName = "DescFiliations";
-            this.sdstxtDescFiliations.DescCodi = null;
-            this.sdstxtDescFiliations.Location = new System.Drawing.Point(190, 228);
-            this.sdstxtDescFiliations.Name = "sdstxtDescFiliations";
-            this.sdstxtDescFiliations.Nom = null;
-            this.sdstxtDescFiliations.NomBBDD = "SecureCore";
-            this.sdstxtDescFiliations.NomCamp = null;
-            this.sdstxtDescFiliations.NomControl = null;
-            this.sdstxtDescFiliations.NomTaula = "Filiations";
-            this.sdstxtDescFiliations.Requerit = false;
-            this.sdstxtDescFiliations.SiEsForana = false;
-            this.sdstxtDescFiliations.Size = new System.Drawing.Size(100, 20);
-            this.sdstxtDescFiliations.TabIndex = 37;
+            this.sdsTexBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.sdsTexBox1.CodeSector = null;
+            this.sdsTexBox1.ColumnName = "DescFiliations";
+            this.sdsTexBox1.DescCodi = null;
+            this.sdsTexBox1.Location = new System.Drawing.Point(202, 286);
+            this.sdsTexBox1.Name = "sdsTexBox1";
+            this.sdsTexBox1.Nom = null;
+            this.sdsTexBox1.NomBBDD = "SecureCore";
+            this.sdsTexBox1.NomCamp = null;
+            this.sdsTexBox1.NomControl = null;
+            this.sdsTexBox1.NomTaula = "Filiations";
+            this.sdsTexBox1.Requerit = false;
+            this.sdsTexBox1.SiEsForana = false;
+            this.sdsTexBox1.Size = new System.Drawing.Size(100, 20);
+            this.sdsTexBox1.TabIndex = 41;
             // 
-            // sdstxtCodeFiliation
+            // sdsTexBox2
             // 
-            this.sdstxtCodeFiliation.BackColor = System.Drawing.Color.LightGreen;
-            this.sdstxtCodeFiliation.CodeSector = null;
-            this.sdstxtCodeFiliation.ColumnName = "CodeFiliation";
-            this.sdstxtCodeFiliation.DescCodi = null;
-            this.sdstxtCodeFiliation.Location = new System.Drawing.Point(190, 176);
-            this.sdstxtCodeFiliation.Name = "sdstxtCodeFiliation";
-            this.sdstxtCodeFiliation.Nom = null;
-            this.sdstxtCodeFiliation.NomBBDD = "SecureCore";
-            this.sdstxtCodeFiliation.NomCamp = null;
-            this.sdstxtCodeFiliation.NomControl = null;
-            this.sdstxtCodeFiliation.NomTaula = "Filiations";
-            this.sdstxtCodeFiliation.Requerit = false;
-            this.sdstxtCodeFiliation.SiEsForana = false;
-            this.sdstxtCodeFiliation.Size = new System.Drawing.Size(100, 20);
-            this.sdstxtCodeFiliation.TabIndex = 38;
+            this.sdsTexBox2.BackColor = System.Drawing.Color.LightGreen;
+            this.sdsTexBox2.CodeSector = null;
+            this.sdsTexBox2.ColumnName = "CodeFiliation";
+            this.sdsTexBox2.DescCodi = null;
+            this.sdsTexBox2.Location = new System.Drawing.Point(188, 208);
+            this.sdsTexBox2.Name = "sdsTexBox2";
+            this.sdsTexBox2.Nom = null;
+            this.sdsTexBox2.NomBBDD = "SecureCore";
+            this.sdsTexBox2.NomCamp = null;
+            this.sdsTexBox2.NomControl = null;
+            this.sdsTexBox2.NomTaula = "Filiations";
+            this.sdsTexBox2.Requerit = false;
+            this.sdsTexBox2.SiEsForana = false;
+            this.sdsTexBox2.Size = new System.Drawing.Size(100, 20);
+            this.sdsTexBox2.TabIndex = 42;
             // 
-            // labDescFiliations
+            // label1
             // 
-            this.labDescFiliations.AutoSize = true;
-            this.labDescFiliations.Location = new System.Drawing.Point(101, 231);
-            this.labDescFiliations.Name = "labDescFiliations";
-            this.labDescFiliations.Size = new System.Drawing.Size(72, 13);
-            this.labDescFiliations.TabIndex = 40;
-            this.labDescFiliations.Text = "DescFiliations";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "DescFiliations";
             // 
-            // labCodeFiliation
+            // label2
             // 
-            this.labCodeFiliation.AutoSize = true;
-            this.labCodeFiliation.Location = new System.Drawing.Point(101, 179);
-            this.labCodeFiliation.Name = "labCodeFiliation";
-            this.labCodeFiliation.Size = new System.Drawing.Size(67, 13);
-            this.labCodeFiliation.TabIndex = 39;
-            this.labCodeFiliation.Text = "CodeFiliation";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "CodeFiliation";
+            // 
+            // sdsTexBox3
+            // 
+            this.sdsTexBox3.BackColor = System.Drawing.Color.LightGreen;
+            this.sdsTexBox3.CodeSector = null;
+            this.sdsTexBox3.ColumnName = "DescFiliations";
+            this.sdsTexBox3.DescCodi = null;
+            this.sdsTexBox3.Location = new System.Drawing.Point(202, 324);
+            this.sdsTexBox3.Name = "sdsTexBox3";
+            this.sdsTexBox3.Nom = null;
+            this.sdsTexBox3.NomBBDD = "SecureCore";
+            this.sdsTexBox3.NomCamp = null;
+            this.sdsTexBox3.NomControl = null;
+            this.sdsTexBox3.NomTaula = "Filiations";
+            this.sdsTexBox3.Requerit = false;
+            this.sdsTexBox3.SiEsForana = false;
+            this.sdsTexBox3.Size = new System.Drawing.Size(100, 20);
+            this.sdsTexBox3.TabIndex = 45;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 46;
+            this.label3.Text = "DescFiliations";
             // 
             // Filiations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(943, 520);
-            this.Controls.Add(this.sdstxtDescFiliations);
-            this.Controls.Add(this.sdstxtCodeFiliation);
-            this.Controls.Add(this.labDescFiliations);
-            this.Controls.Add(this.labCodeFiliation);
+            this.Controls.Add(this.sdsTexBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sdsTexBox1);
+            this.Controls.Add(this.sdsTexBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Name = "Filiations";
             this.Text = "Filiations";
             this.Load += new System.EventHandler(this.Filiations_Load);
             this.Controls.SetChildIndex(this.btnInsert, 0);
             this.Controls.SetChildIndex(this.btnUpdate, 0);
-            this.Controls.SetChildIndex(this.labCodeFiliation, 0);
-            this.Controls.SetChildIndex(this.labDescFiliations, 0);
-            this.Controls.SetChildIndex(this.sdstxtCodeFiliation, 0);
-            this.Controls.SetChildIndex(this.sdstxtDescFiliations, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.sdsTexBox2, 0);
+            this.Controls.SetChildIndex(this.sdsTexBox1, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.sdsTexBox3, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,9 +154,11 @@
 
         #endregion
 
-        private SdsTexBox.SdsTexBox sdstxtDescFiliations;
-        private SdsTexBox.SdsTexBox sdstxtCodeFiliation;
-        private System.Windows.Forms.Label labDescFiliations;
-        private System.Windows.Forms.Label labCodeFiliation;
+        private SdsTexBox.SdsTexBox sdsTexBox1;
+        private SdsTexBox.SdsTexBox sdsTexBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private SdsTexBox.SdsTexBox sdsTexBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
